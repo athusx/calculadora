591c49e9a2f5e4198febefe3eb1e0abadb23db99
athusx <110572717+athusx@users.noreply.github.com>
2022-12-07 17:21:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
     function Limpar(id1, id2){
-        document.getElementById("id1").value="";
-        document.getElementById("id2").value="";
+        document.getElementById("id1").value=" ";
+        document.getElementById("id2").value=" ";
     }
 
     function Calcular(numero1, numero2, operacao){
         var num1 = document.getElementById(numero1).value;
         var num2 = document.getElementById(numero2).value;
         var operador = document.getElementById(operacao).options[document.getElementById(operacao).selectIndex].value;
-        var expressao = num1 + operador + num2;
+        var expressao = num1 + operacao + num2;
         resultado = eval(expressao);
         document.getElementById(saida).innerHTML = resultado;
 
