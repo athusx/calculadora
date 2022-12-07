8b95d0f5a6b1e034fb8f1e688039f797c655b47d
athusx <110572717+athusx@users.noreply.github.com>
2022-12-07 20:40:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 
-    function Limpar(id1, id2){
-        document.getElementById("id1").value=" ";
-        document.getElementById("id2").value=" ";
+    function Limpar(valor1, valor2, saida){
+        document.getElementById(valor1).value='';
+        document.getElementById(valor2).value='';
+        document.getElementById(saida).innerHTML='';
     }
-
+ 
     function Calcular(numero1, numero2, operacao){
         var num1 = document.getElementById(numero1).value;
         var num2 = document.getElementById(numero2).value;
-        var operador = document.getElementById(operacao).options[document.getElementById(operacao).selectIndex].value;
-        var expressao = num1 + operacao + num2;
-        resultado = eval(expressao);
-        document.getElementById(saida).innerHTML = resultado;
+        var operador = document.getElementById(operacao).options[document.getElementById(operacao).selectedIndex].value;
+        var expressao = num1 + operador + num2;
+        var ap;
+        var resultado = eval(expressao);
+        if(operador =='+'){
+                ap =  'A soma de ' +num1+' e ' +num2+ ' é ' +resultado; 
+       }
+       if(operador == '-'){
+            ap = 'A diferença entre ' +num1+ ' e ' +num2+ ' é ' +resultado;
+       }
+       if(operador == '*'){
+            ap = 'O produto' +num1+ ' por ' +num2+ ' é ' +numero;
+       }
+       if(operador == '/'){
+         ap = 'O quociente de ' +num1+ ' dividido por ' +num2+ ' é ' +resultado; 
+       }
 
+        document.getElementById("saida").innerHTML=ap;
+        
+       
+      
     }
+    
+
+    
